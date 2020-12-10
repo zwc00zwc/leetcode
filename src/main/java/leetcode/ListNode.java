793d82255ff56c34fecfa4f3dbf6dbe9e22b19a7
zhengwenchao <zhwch@mallcai.com>
2020-12-10 11:17:34 +0800

    init

---
+++ src/main/java/leetcode/ListNode.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * @Author: siskin_zh
+ * @Date: 2020 2020-12-10 11:12
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
